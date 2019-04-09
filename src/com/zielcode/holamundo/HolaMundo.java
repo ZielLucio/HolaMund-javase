ac58107348f5b3184450f836ccbb49b354cf5188
Ziel Lucio <raziel_lucio@icloud.com>
2019-04-08 23:01:39 -0500

    Clase 24

--- src/com/zielcode/holamundo/HolaMundo.java
+++ src/com/zielcode/holamundo/HolaMundo.java
@@ -1,10 +1,141 @@
 package com.zielcode.holamundo;
 
 public class HolaMundo {
-
+	//upper Camel Case
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo :)");
+		
+		//Enteros
+		byte edad = 127;
+		short year = -32768;
+		int id_user = 1001;
+		long id_twitter = 4545645646456456456L;
+		
+		//Punto Flotante
+		float diametro = 34.25F;
+		double precio = 12345.12345489456421;
+		
+		//Texto
+		char genero = 'M';
+		
+		//Logico
+		boolean isVisible = true;
+		boolean funciona = false;
+		
+		
+		int variable = 2;
+		int _variable = 3;
+		int $variable = 4;
+		int variable1 = 5;
+		
+		//Constantes
+		int VALOR = 0;
+		int VALOR_MAXIMO = 1;
+		
+		//Lower camel case
+		int minValor = 4;
+		
+		byte b = 6;
+		short s = b;
+		
+		b = (byte)s;
+		
+		int i = 1;
+		double d = 2.5;
+		
+		i = (int) d;
+		
+		int codigo = 97;
+		char codigoASCII = (char)codigo;
+		
+		short numero = 300;
+		byte numeroBYTE = (byte)numero;
+		
+		//Arrays
+		//Declaracion de array
+		
+		int[] arregloInt = new int[3];
+		double arregloDouble[]; 
+		
+		int[][] array2D = new int[2][3]; //Caben 6 cupcakes
+		int[][][] array3D = new int[3][3][2]; //caben 18 cookies
+		int[][][][] array4D = new int [1][2][3][4];
+		
+		char[][] days = { {'M', 'T', 'W'}, {'M', 'T', 'W'}};
+		//char[][][] days = { {'a', 'b', 'c'}, {'a', 'b', 'c'}, {'a', 'b', 'c'}};
+		
+		
+		char[] names = new char [4];
+		names[0] = 'h';
+		names[1] = 'o';
+		names[2] = 'l';
+		names[3] = 'a';
+			
+		
+		System.out.println(names[0]);
+		System.out.println(names[1]);
+		System.out.println(names[2]);
+		System.out.println(names[3]);
+		
+		char[][][][] monkey = new char [2][3][2][2];
+		monkey [1][0][0][1] = 'm';
+		
+		System.out.println(monkey [1][0][0][1]);
+		
+		
+		//Operadores Aritmeticos
+		int a = 1;
+		int aa = a+a;
+		
+		System.out.println("El valor de: " + aa);
+		
+		double x = 2.56;
+		int y = 9;
+		
+		float w = (float)x + y;
+		System.out.println(w*2);
+		
+		double k = 4/0.00002;
+		System.out.println(k);
+		
+		
+		System.out.println(7%2);
+		
+		double f = 2;
+		int g = 3;
+		f %= g;
+		
+		System.out.println(f);
+		
+		int l = 3;
+		//l++;
+		++l;
+		System.out.println(l);
+		
+		
+		//Operadores de Equidad
+		int q = 8;
+		int p = 5;
+		
+		System.out.println(q!=p);
+		
+		//Operadores relacionales
+		System.out.println("q > p -> " +  (q>p));
+		System.out.println("q < p -> " +  (q<p));
+		System.out.println("q = p -> " +  (q==p));
+		
+		System.out.println("q >= p -> " +  (q>=p));
+		System.out.println("q <= p -> " +  (q<=p));
+		
+		//operadores logicos
+		boolean n = false;
+		boolean m = true;
+		
+		System.out.println("a && b ->" + (n && m));
+		System.out.println("a || b ->" + (n || m));
+		System.out.println("!n ->" + (!n));
+		System.out.println("!m ->" + (!m));
 	}
 
 }
